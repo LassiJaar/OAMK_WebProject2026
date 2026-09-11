ad043af430dfc1d1539b4c36e4eca228cc2c7ba5
LassiJaara <Lassi.jaara2004@gmail.com>
2026-09-11 11:15:29 +0300

    MovieCards + NowPlaying
    Created a section for NowPlaying and the MovieCard object for listing movies

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,15 +3,16 @@ import Topbar from './Topbar';
 import styles from './Main.module.css';
 import Search from './Search';
 import Clubs from './Clubs';
+import NowPlaying from './NowPlaying';
 
 const Main = () => {
   return (
     <div>
       <Topbar></Topbar>
-
+      
       <div className={styles.content}>
         <Routes>
-          <Route path="/"></Route>
+          <Route path="/" element={<NowPlaying></NowPlaying>}></Route>
           <Route path="/search" element={<Search></Search>}></Route>
           <Route path="/clubs" element={<Clubs></Clubs>}></Route>
           <Route path="/clubs/:id" element={<p>single</p>}></Route>
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router';
+import styles from './MovieCard.module.css';
+
+const MovieCard = ({ movie }) => {
+  return (
+    <div className={styles.card}>
+      <Link to={`/movies/${movie.movie_id}`}>
+        <div>
+          <p>{movie.title}</p>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Image.svg?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=original"
+            alt={movie.movie_id}
+          ></img>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default MovieCard;
